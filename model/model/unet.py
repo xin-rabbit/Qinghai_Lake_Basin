b95974e3894f96c19a327724a542eccb8f80c8b2
Luna <2140931620@qq.com>
2026-03-16 13:23:25 +0800

    model

---
+++ model/model/unet.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def conv3x3_bn_relu(in_channels, out_channels, dropout=False):
+    layers = [
+        nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+    ]
+    if dropout:
+        layers.append(nn.Dropout2d(0.5))
+    return nn.Sequential(*layers)
+
+class unet(nn.Module):
+    def __init__(self, num_bands):
+        super(unet, self).__init__()
+        self.num_bands = num_bands
+        self.up = nn.Upsample(scale_factor=2, mode='nearest')
+        self.down_conv1 = conv3x3_bn_relu(num_bands, 16, dropout=True)
+        self.down_conv2 = conv3x3_bn_relu(16, 32, dropout=True)
+        self.down_conv3 = conv3x3_bn_relu(32, 64, dropout=True)
+        self.down_conv4 = conv3x3_bn_relu(64, 128, dropout=True)
+        self.up_conv1 = conv3x3_bn_relu(192, 64, dropout=True)
+        self.up_conv2 = conv3x3_bn_relu(96, 48, dropout=True)
+        self.up_conv3 = conv3x3_bn_relu(64, 32)
+        self.outp = nn.Sequential(
+                nn.Conv2d(32, 1, kernel_size=3, padding=1),
+                nn.Sigmoid()) 
+
+    def forward(self, x):  ## input size: 6x256x256
+        ## encoder part
+        x1 = self.down_conv1(x)              
+        x1 = F.avg_pool2d(input=x1, kernel_size=2)  # 16x128x128
+        x2 = self.down_conv2(x1)              
+        x2 = F.avg_pool2d(input=x2, kernel_size=2) # 32x64x64
+        x3 = self.down_conv3(x2)              
+        x3 = F.avg_pool2d(input=x3, kernel_size=2) # 64x32x32
+        x4 = self.down_conv4(x3)              
+        x4 = F.avg_pool2d(input=x4, kernel_size=2) # 128x16x16
+        ## decoder part
+        x4_up = torch.cat([self.up(x4), x3], dim=1)  # (128+64)x32x32
+        x3_up = self.up_conv1(x4_up)  # 64x32x32
+        x3_up = torch.cat([self.up(x3_up), x2], dim=1)  # (64+32)x64x64
+        x2_up = self.up_conv2(x3_up)  # 48x64x64
+        x2_up = torch.cat([self.up(x2_up), x1], dim=1)  # (48+16)x128x128
+        x1_up = self.up_conv3(x2_up)    # 32x128x128
+        x1_up = self.up(x1_up)        # 32x256x256
+        logits = self.outp(x1_up)
+        return logits          
